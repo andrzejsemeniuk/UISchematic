10a93dc3c6b5748f13184254dffefa75124ddefe
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2017-07-14 08:33:11 -0700

    Initial Commit

---
+++ UISchematic/UISchematic.h
@@ -0,0 +1,19 @@
+//
+//  UISchematic.h
+//  UISchematic
+//
+//  Created by andrzej semeniuk on 7/14/17.
+//  Copyright © 2017 Andrzej Semeniuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UISchematic.
+FOUNDATION_EXPORT double UISchematicVersionNumber;
+
+//! Project version string for UISchematic.
+FOUNDATION_EXPORT const unsigned char UISchematicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UISchematic/PublicHeader.h>
+
+
